ce7ffde3801c836987564254c7e003eb4633e6e0
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2021-08-02 23:18:20 -0500

    Change data model name from BookApi to MovieApi

--- app/src/main/java/com/alfonso/myapplication/repository/api/model/UpcomingRequest.kt
+++ app/src/main/java/com/alfonso/myapplication/repository/api/model/UpcomingRequest.kt
@@ -3,4 +3,4 @@ package com.alfonso.myapplication.repository.api.model
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class UpcomingRequest(val page: Int, val results: List<BookApi>)
+data class UpcomingRequest(val page: Int, val results: List<MovieApi>)
